fe62a7816cecabcf47d54ff2d51837d48634ae9e
szolton <szolton.designs@gmail.com>
2024-05-13 20:59:45 -0400

    added stuff

--- controllers/controller.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-// This is the 'get' route
-router.get('/', async (req, res) => {
-    // TO DO: Render index.html
-    res.sendFile
-});
-
-module.exports = router;

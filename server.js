10883944bc559d65d6a1ad84d7e00bda4c9a3ca1
szolton <szolton.designs@gmail.com>
2024-05-14 21:21:46 -0400

    error fixes

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 // Importing required modules
 const path = require('path');
 const express = require('express');
-const exphbs = ('express-handlebars');
+const exphbs = require('express-handlebars'); // Corrected import
 
-const routes = require('./controllers/api/controller');
+const routes = require('./controllers/api/index.js');
+
+// Define a middleware function
+const myMiddleware = (req, res, next) => {
+  // Middleware logic here
+  console.log('Middleware executed');
+  next(); // Call next to pass control to the next middleware or route handler
+};
 
 // Creating an Express application
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Setting up Handlebars as the view engine
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Using the middleware function
+app.use(myMiddleware);
 
 // Serving static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));

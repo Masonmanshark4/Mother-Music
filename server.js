1a2ed90c670a892514702cd50d9c208da5258e91
kaileesegarra <158116690+kaileesegarra@users.noreply.github.com>
2024-05-18 13:25:43 -0400

    server.js link fixes

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // middleware
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'Public')));
+//app.use(favicon(path.join(__dirname, 'Public', 'favicon.ico')));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true

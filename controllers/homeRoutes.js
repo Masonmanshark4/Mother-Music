bc13541f34773014266870ebcf2671c13b528a5b
Masonmanshark4 <149852525+Masonmanshark4@users.noreply.github.com>
2024-05-20 18:24:26 -0400

    change back to fix error

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -95,7 +95,7 @@ router.get('/post/:id', (req, res) => {
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
-        res.redirect('/dashboard');
+        res.redirect('/');
         return;
     }
 
@@ -104,7 +104,7 @@ router.get('/login', (req, res) => {
 
 router.get('/signup', (req, res) => {
     if (req.session.loggedIn) {
-        res.redirect('/dashboard');
+        res.redirect('/');
         return;
     }
 
